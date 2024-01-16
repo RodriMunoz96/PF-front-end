1bca18f76ee1f3add1e443e5dc4b67b575f13677
RodriMunoz96 <rodrigo.gaston.munoz@gmail.com>
2024-01-16 02:19:40 -0300

    mensaje de validación en espera

--- src/components/ViewParent/AsideParent/AsideParent.jsx
+++ src/components/ViewParent/AsideParent/AsideParent.jsx
@@ -1,8 +1,8 @@
-import { useEffect, useState } from "react";
+import { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import Links from "./Links/Links";
 import style from "./asideParent.module.css";
-import { Collapse } from "react-bootstrap";
+// import { Collapse } from "react-bootstrap";
 import myProfile from "../../../Img/imgViewParent/iconMyProfile.png";
 import myChildren from "../../../Img/imgViewParent/iconChild.png";
 import myBook from "../../../Img/imgViewParent/iconBook.png";
@@ -15,53 +15,59 @@ const user = sessionStorage;
 const parentId = sessionStorage.getItem("parentId");
 
 function AsideParent() {
-     const dispatch = useDispatch();
-     useEffect(() => { dispatch(getParent(parentId)) }, [dispatch]);
-     const parent = useSelector((state) => state.parent);
-     const [open, setOpen] = useState(false);
+  const dispatch = useDispatch();
+  useEffect(() => {
+    dispatch(getParent(parentId));
+  }, [dispatch]);
+  const parent = useSelector((state) => state.parent);
+  // const [open, setOpen] = useState(false);
 
-     return (
-          <aside className={style.container_aside}>
-               <div className={style.container_dad_name}>
-                    <h1>
-                         BIENVENIDO A TU SESION <br /> <br /> {user ? user.nombre : ""}
-                    </h1>
-               </div>
-               <hr />
-               <div className={style.container_links}>
-                    <Links url={"myProfile"} img={myProfile} name={"Mi Perfil"} />
-                    {parent.validate && (
-                         <Links url={"myChildren"} img={myChildren} name={"Mis hijos"} />
-                    )}
-                    {parent.validate && (
-                         <Links url={"studentForm"} img={myBook} name={"Incribir a mi hijo"} />
-                    )}
-                    {parent.validate && (
-                         <Links
-                              url={"comentario"}
-                              img={comentarios}
-                              name={"Dejanos tu comentario"}
-                         />
-                    )}
-                    {!parent.validate && (
-                         <div>
-                              <Collapse in={open}>
-                                   <div>
-                                        <p>
-                                             Su perfil está en proceso de validación, una vez el
-                                             administrador lo valide, podrá inscribir a sus estudiantes.
-                                        </p>
-                                   </div>
-                              </Collapse>
-                              <h5 onClick={() => setOpen(!open)} style={{ cursor: "pointer" }}>
-                                   {!open ? "Ver nota" : "Cerrar nota"}
-                              </h5>
-                         </div>
-                    )}
-                    <LogoutButton img={logout} className={style.container_links} />
-               </div>
-          </aside>
-     );
+  return (
+    <aside className={style.container_aside}>
+      <div className={style.container_dad_name}>
+        <h1>
+          BIENVENIDO A TU SESION <br /> <br /> {user ? user.nombre : ""}
+        </h1>
+      </div>
+      <hr />
+      <div className={style.container_links}>
+        <Links url={"myProfile"} img={myProfile} name={"Mi Perfil"} />
+        {parent.validate && (
+          <Links url={"myChildren"} img={myChildren} name={"Mis hijos"} />
+        )}
+        {parent.validate && (
+          <Links url={"studentForm"} img={myBook} name={"Incribir a mi hijo"} />
+        )}
+        {parent.validate && (
+          <Links
+            url={"comentario"}
+            img={comentarios}
+            name={"Dejanos tu comentario"}
+          />
+        )}
+
+        <LogoutButton img={logout} className={style.container_links} />
+      </div>
+    </aside>
+  );
 }
 
 export default AsideParent;
+
+/* {
+  !parent.validate && (
+    <div>
+      <Collapse in={open}>
+        <div>
+          <p>
+            Su perfil está en proceso de validación, una vez el administrador lo
+            valide, podrá inscribir a sus estudiantes.
+          </p>
+        </div>
+      </Collapse>
+      <h5 onClick={() => setOpen(!open)} style={{ cursor: "pointer" }}>
+        {!open ? "Ver nota" : "Cerrar nota"}
+      </h5>
+    </div>
+  );
+} */
--- src/components/ViewParent/MainComponents/MyProfile/MyProfile.jsx
+++ src/components/ViewParent/MainComponents/MyProfile/MyProfile.jsx
@@ -7,52 +7,81 @@ import { getParent } from "../../../../redux/actions/actions-parents";
 //import productos from "../../../../../productos.json";
 
 function MyProfile() {
-     const dispatch = useDispatch();
-     const parentId = useSelector((state) => state.parentId);
-     const parent = useSelector((state) => state.parent);
-     const filteredParentId = parentId?.parentDetails?.[0]?.id;
+  const dispatch = useDispatch();
+  const parentId = useSelector((state) => state.parentId);
+  const parent = useSelector((state) => state.parent);
+  const filteredParentId = parentId?.parentDetails?.[0]?.id;
 
-     useEffect(() => {
-          if (filteredParentId) {
-               dispatch(getParent(filteredParentId));
-               sessionStorage.setItem("parentId", filteredParentId);
-          }
-     }, [dispatch, filteredParentId]);
+  useEffect(() => {
+    if (filteredParentId) {
+      dispatch(getParent(filteredParentId));
+      sessionStorage.setItem("parentId", filteredParentId);
+    }
+  }, [dispatch, filteredParentId]);
 
-     return (
-          <div>
-               <h1>Mi perfil</h1>
-               <div>
-                    <br />
-                    <div>
-                         {parentId.parentDetails && parentId.parentDetails.length > 0 ? (
-                              <div>
-                                   <img src={parent.fotoDocumento} className={style.docImage}></img>
-                                   <p>Nombre: {parent.name}</p>
-                                   <p>Apellido: {parent.lastName}</p>
-                                   <p>Documento: {parent.idDoc}</p>
-                                   <p>Email: {parent.email}</p>
-                                   <p>Teléfono personal: {parent.contactCellphone}</p>
-                                   <p>Teléfono del lugar de trabajo: {parent.jobTelephone}</p>
-                                   <p>Domicilio: {parent.address}</p>
-                                   <p>Dirección del trabajo: {parent.jobAddress}</p>
-                              </div>
-                         ) : (
-                              <div>
-                                   <p>Aún no te has registrado como padre</p>
-                                   <Button
-                                        as={NavLink}
-                                        to={"/formParent"}
-                                        className={style.link_to_parent}
-                                   >
-                                        Llenar datos
-                                   </Button>
-                              </div>
-                         )}
-                    </div>
-               </div>
-          </div>
-     );
+  return (
+    <div className={style.main_container}>
+      <h1 className={style.title}>Datos personales</h1>
+      <div>
+        <br />
+        <div>
+          {parentId.parentDetails && parentId.parentDetails.length > 0 ? (
+            <div className={style.profile_container}>
+              <img src={parent.fotoDocumento} className={style.docImage}></img>
+              <p>
+                <strong>Nombre:</strong> {parent.name}
+              </p>
+              <p>
+                <strong>Apellido:</strong> {parent.lastName}
+              </p>
+              <p>
+                <strong>Documento:</strong> {parent.idDoc}
+              </p>
+              <p>
+                <strong>Email:</strong> {parent.email}
+              </p>
+              <p>
+                <strong>Teléfono personal:</strong> {parent.contactCellphone}
+              </p>
+              <p>
+                <strong>Teléfono del lugar de trabajo:</strong>{" "}
+                {parent.jobTelephone}
+              </p>
+              <p>
+                <strong>Domicilio:</strong> {parent.address}
+              </p>
+              <p>
+                <strong>Dirección del trabajo:</strong> {parent.jobAddress}
+              </p>
+            </div>
+          ) : (
+            <div>
+              <h5>Aún no te has registrado como padre</h5>
+
+              <Button
+                as={NavLink}
+                to={"/formParent"}
+                className={style.link_to_parent}
+              >
+                Llenar datos
+              </Button>
+            </div>
+          )}
+          <br />
+          {parentId.parentDetails &&
+            parentId.parentDetails.length > 0 &&
+            !parent.validate && (
+              <div className={style.validation_message}>
+                <h5>
+                  Su perfil está en proceso de validación; una vez el
+                  administrador lo valide, podrá inscribir a sus estudiantes.
+                </h5>
+              </div>
+            )}
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default MyProfile;
